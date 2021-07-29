99290a39478a1cc42467207e22a1e4f0c13bcae1
Fernando Branca <Fernando Branca@LAPTOP-6MHDOV0V>
2021-07-29 17:18:44 -0300

    incorporando modelo

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from models.random_predictor import evaluate_sample
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-		return '<h1>Hello! flask app</h1>'
+		result=evaluate_sample()
+		if result: 
+			return '<h1>anda</h1>'
+		else: 
+			return '<h1>no anda</h1>' 
+		
+
---
+++ models/random_predictor.py
@@ -0,0 +1,5 @@
+import random
+
+
+def evaluate_sample():
+	return bool(random.getrandbits(1))
